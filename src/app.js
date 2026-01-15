269e3941dab7c9f840b82071b4ad25030d61c135
judossantos13 <judossantos13@gmail.com>
2026-01-15 09:20:38 +0100

    update js search-function with API

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
+function refreshTemperature(response) {
+  let temperatureElement = document.querySelector(".degrees");
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "76ab2ecoa4e0c3c3807ad4cff1b5696t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unitsmetric`;
+  axios.get(apiUrl).then(refreshTemperature);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#cityInput");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searhFormElement = document.querySelector("#search-form");
